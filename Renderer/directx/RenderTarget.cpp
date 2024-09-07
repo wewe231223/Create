9d9e62a56c6fc2c8d8dc8a7412555ba22832c56e
wewe231223@Desktop <wewe231223@kpu.ac.kr>
2024-09-07 13:22:32 +0900

    09-07-2

--- Renderer/directx/RenderTarget.cpp
+++ Renderer/directx/RenderTarget.cpp
@@ -1,21 +1,14 @@
 #include "pch.h"
 #include "directx/RenderTarget.h"
 
-RenderTargetGroup::RenderTargetGroup(ComPtr<ID3D12Device> device, UINT bufferCount)
-{
-	D3D12_DESCRIPTOR_HEAP_DESC rtvHeapDesc = {};
-	rtvHeapDesc.NumDescriptors = bufferCount;
-	rtvHeapDesc.Type = D3D12_DESCRIPTOR_HEAP_TYPE_RTV;
-	rtvHeapDesc.Flags = D3D12_DESCRIPTOR_HEAP_FLAG_NONE;
-	rtvHeapDesc.NodeMask = 0;
-	CheckHR(device->CreateDescriptorHeap(&rtvHeapDesc, IID_PPV_ARGS(&mRtvHeap)));
 
-	D3D12_DESCRIPTOR_HEAP_DESC dsvHeapDesc = {};
-	dsvHeapDesc.NumDescriptors = 1;
-	dsvHeapDesc.Type = D3D12_DESCRIPTOR_HEAP_TYPE_DSV;
-	dsvHeapDesc.Flags = D3D12_DESCRIPTOR_HEAP_FLAG_NONE;
-	dsvHeapDesc.NodeMask = 0;
-	CheckHR(device->CreateDescriptorHeap(&dsvHeapDesc, IID_PPV_ARGS(&mDsvHeap)));
+RenderTargetGroup::RenderTargetGroup(ComPtr<ID3D12Device> device, std::vector<ComPtr<ID3D12Resource>>& rts, ComPtr<ID3D12Resource>& ds)
+{
+	mRenderTargets = std::move(rts);
+	mDepthStencil = std::move(ds);
+
+
+
 }
 
 RenderTargetGroup::~RenderTargetGroup()
--- Renderer/directx/RenderTarget.h
+++ Renderer/directx/RenderTarget.h
@@ -3,33 +3,38 @@
 
 // MRT 같은 여러개의 패스를 사용할 때를 고려해서 설계해보자 
 class RenderTargetGroup {
-	class RenderTarget {
-	public:
-
-	};
-
-	class DepthStencil {
-
-	};
 public:
-	RenderTargetGroup(ComPtr<ID3D12Device> device,UINT bufferCount);
+	/// <summary>
+	/// 이 함수는 매개변수로 받아온 rts, ds 에 대해 std::move 를 호출합니다. 
+	/// 
+	/// </summary>
+	/// <param name="device"></param>
+	/// <param name="rts"></param>
+	/// <param name="ds"></param>
+	RenderTargetGroup(ComPtr<ID3D12Device> device,std::vector<ComPtr<ID3D12Resource>>& rts,ComPtr<ID3D12Resource>& ds);
 	~RenderTargetGroup();
 
+	// 하나만 
+	void Reset(UINT index);
+	// 전부 
+	void Reset();
+
 	// 하나만
-	void SetRenderTarget(ComPtr<ID3D12GraphicsCommandList> commandList, UINT bufferIndex);
+	void SetRenderTarget(ComPtr<ID3D12GraphicsCommandList> commandList, UINT index);
 	// 전부 
 	void SetRenderTarget(ComPtr<ID3D12GraphicsCommandList> commandList);
 
 	// 하나만 
-	void Clear(ComPtr<ID3D12GraphicsCommandList> commandList, UINT bufferIndex);
+	void Clear(ComPtr<ID3D12GraphicsCommandList> commandList, UINT index);
 	// 전부 
 	void Clear(ComPtr<ID3D12GraphicsCommandList> commandList);
 
 
 private:
-	ComPtr<ID3D12DescriptorHeap> mRtvHeap{ nullptr };
-	ComPtr<ID3D12DescriptorHeap> mDsvHeap{ nullptr };
+	ComPtr<ID3D12DescriptorHeap>		mRtvHeap{ nullptr };
+	ComPtr<ID3D12DescriptorHeap>		mDsvHeap{ nullptr };
 
-	
+	std::vector<ComPtr<ID3D12Resource>> mRenderTargets{};
+	ComPtr<ID3D12Resource>				mDepthStencil{ nullptr };
 };
 	
