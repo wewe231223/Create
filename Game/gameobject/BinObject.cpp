20b4bdc76cc8f79cf6a275a9d10620d071f72cc9
wewe231223@Laptop <wewe231223@kpu.ac.kr>
2024-10-12 00:07:45 +0900

    내일 이어서
    BinObject TODO 참고.

--- Game/gameobject/BinObject.cpp
+++ Game/gameobject/BinObject.cpp
@@ -1,19 +1,65 @@
 #include "pch.h"
 #include "Game/gameobject/BinObject.h"
 
+// 내일 실제 데이터 넣어보고 렌더링 해보자
+// 에셋 바이너라이저는 단순 정점 정보만 올리고 재질 정보는 별도로 바인딩 하는 것으로 한다. 
+
 BinObject::BinObject(std::shared_ptr<ResourceManager> resourceMgr, const fs::path& binPath)
 {
-	std::string modelName = binPath.stem().string();
-
-	auto model = resourceMgr->GetModel(modelName);
-
-	if (model == nullptr) {
-
-	}
 
 	
+}
+
+BinObject::BinObject(std::shared_ptr<ResourceManager> resourceMgr, std::ifstream& file)
+{
+
 }
 
 BinObject::~BinObject()
 {
 }
+
+BinObject* BinObject::GetChild(UINT dfsIndex)
+{
+	if (dfsIndex == 0) {
+		return this;
+	}
+
+	BinObject* ret = nullptr;
+	UINT index = dfsIndex;
+
+
+	for (auto& child : mChildObjects) {
+		ret = GetChildInternal(dfsIndex);
+		if (ret != nullptr) {
+			break;
+		}
+	}
+
+
+	return ret;
+}
+
+BinObject* BinObject::GetChildInternal(UINT& dfsIndex)
+{
+	if (dfsIndex == 0) {
+		return this;
+	}
+
+	dfsIndex -= 1;
+
+	BinObject* ret = nullptr;
+
+	for (auto& child : mChildObjects) {
+		ret = GetChildInternal(dfsIndex);
+
+		if (ret != nullptr) {
+			break;
+		}
+	}
+
+
+	return ret;
+}
+
+
--- Game/gameobject/BinObject.h
+++ Game/gameobject/BinObject.h
@@ -1,10 +1,15 @@
 #pragma once 
 
+// Material 은 별도로 설정해야한다. 
 class BinObject : public GameObject {
 public:
 	BinObject(std::shared_ptr<ResourceManager> resourceMgr,const fs::path& binPath);
+	BinObject(std::shared_ptr<ResourceManager> resourceMgr, std::ifstream& file);
 	virtual ~BinObject();
 public:
+	BinObject* GetChild(UINT dfsIndex);
 private:
-
+	BinObject* GetChildInternal(UINT& dfsIndex);
+private:
+	std::vector<BinObject> mChildObjects{};
 };
