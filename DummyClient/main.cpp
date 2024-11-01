3e33999fef253ef43d6e50f455285327b7bf9ab0
KSJTUK <lastbabo2480@tukorea.ac.kr>
2024-11-02 01:24:06 +0900

    패킷 송수신용 더미 클라이언트 프로젝트 생성

---
+++ DummyClient/main.cpp
@@ -0,0 +1,6 @@
+#include "pch.h"
+
+int main(int argc, char* argv[])
+{
+    std::cout << "Hello DummyClient" << std::endl;
+}
---
+++ DummyClient/pch.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <WinSock2.h>
+#include <WS2tcpip.h>
+
+#pragma comment(lib, "ws2_32.lib")
+
+#include <Windows.h>
+
+#include <iostream>
