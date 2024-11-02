4783b5ded7ce235d3919bf1f759ee61d6d4842c9
Jeong071 <rldudwjd123@naver.com>
2024-11-02 21:30:37 +0900

    11-02 ClientClass생성
    ClientClass생성

---
+++ Server/Client.h
@@ -0,0 +1,44 @@
+#pragma once
+
+
+
+
+class Client {
+public:
+	Client();
+	~Client();
+
+	bool InitializeClient(SOCKET socket);
+	void ShutdownClient();
+	BYTE GetId();
+
+	std::mutex& GetRecvMutex();
+	std::mutex& GetSendMutex();
+
+	void WakeSendThread();
+	void SendWorker();
+	void RecvWorker();
+	void ReadFromRecvBuffer(class PIStream& stream);
+
+	void SendLoginPass(bool success);
+	void SendPlayerExit();
+	void SendPlayerEntered();
+	void SendPlayerReady(BYTE color);
+	void SendGameEnd(bool win);
+	void SendPlayerInfo(float hp, const DirectX::SimpleMath::Vector3& position, const DirectX::SimpleMath::Quaternion rotationParent, const DirectX::SimpleMath::Quaternion rotationChild);
+	void SendBulletInfo(short bulletIndex, const DirectX::SimpleMath::Vector3& position);
+
+
+private:
+	SOCKET					mSocket;
+	char					mIp[16];
+	unsigned short			mPort;
+	std::thread				mSendThread;
+	std::thread				mRecvThread;
+	std::mutex				mSendLock;
+	std::mutex				mRecvLock;
+	std::condition_variable mSendConditionVar;
+
+	
+
+};
--- Server/pch.h
+++ Server/pch.h
@@ -3,6 +3,9 @@
 #include <iostream>
 #include <WinSock2.h>
 #include <thread>
+#include <mutex>
 
-
+#include "../Renderer/external/Include/DirectXTK12/SimpleMath.h"
 #pragma comment(lib, "ws2_32.lib")
+
+
